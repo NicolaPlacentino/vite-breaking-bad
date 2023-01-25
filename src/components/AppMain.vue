e5b26e26838d54da61dab8113901f393966dd788
NicolaPlacentino <nyko0921@gmail.com>
2023-01-25 15:15:11 +0100

    added selectComponent

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,34 @@
 <script>
 import Card from './Card.vue';
+import SelectComponent from './SelectComponent.vue';
 import { store } from '../store';
 
 export default {
     name: 'AppMain',
-    components: { Card },
+    components: { Card, SelectComponent },
     data() {
         return {
-            store
+            store,
+            pokemonTypes: [
+                "Water",
+                "Ground",
+                "Bug",
+                "Flying",
+                "Psychic",
+                "Grass",
+                "Ice",
+                "Ghost",
+                "Poison",
+                "Fighting",
+                "Electric",
+                "Rock",
+                "Dark",
+                "Fairy",
+                "Fire",
+                "Normal",
+                "Dragon",
+                "Steel"
+            ]
         }
     }
 }
@@ -15,6 +36,8 @@ export default {
 
 <template>
     <div class="container">
+
+        <!-- card section -->
         <div class="row row-cols-5 justify-content-around">
             <div class="col-auto mb-5" v-for="pokemon in store.Pokemons" :key="pokemon.number">
 
@@ -23,6 +46,10 @@ export default {
 
             </div>
         </div>
+
+        <h5 class="text-center">Filtra per tipo</h5>
+        <SelectComponent :pokemonTypes="this.pokemonTypes" :name="'pokemon-types'"></SelectComponent>
+
     </div>
 </template>
 
---
+++ src/components/SelectComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'SelectComponent',
+    props: {
+        pokemonTypes: Array,
+        name: String,
+    }
+}
+</script>
+
+<template>
+    <select :name="name" id="" class="form-select">
+        <option selected value="">-</option>
+        <option v-for="pokemonType in this.pokemonTypes" :value="pokemonType">{{ pokemonType }}</option>
+    </select>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables' as *;
+
+select {
+    width: 50%;
+    margin: 0 auto;
+    margin-bottom: 50px;
+}
+</style>
